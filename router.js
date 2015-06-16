6f0e0f363e4c58b5d69de358856c8e89b21c4dbb
ianofearth <ian.anthony.bevel>
2015-06-16 10:48:59 -0700

    made auctions page home page

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 Auction.Router.map(function(){
-  this.resource('auctions');
+  this.resource('auctions', {path: '/'});
   this.resource('auction', {path: '/auction/:auction_id'}, function() {
     this.resource('new-item');
 
